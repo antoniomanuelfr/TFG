886409830364fe6614ed31d5e21dc87bdd62024e
Antonio Fresneda <antonio.fresneda@wazuh.com>
2021-06-19 17:09:43 +0200

    Remove files and rename other files

--- src/decision_tree_regressor.py
+++ src/decision_tree_regressor.py
@@ -14,6 +14,39 @@ from sklearn.model_selection import GridSearchCV, KFold
 from sklearn.tree import DecisionTreeRegressor, plot_tree
 import matplotlib.pyplot as plt
 
+
+def preprocessing(X_train, Y_train, X_test, Y_test):
+    """Function to perform the preprocessing for Decision Trees.
+    Params:
+        X_train: Train dataset.
+        Y_train: Labels for training dataset.
+        X_test: Test dataset.
+        Y_test: Labels for test dataset.
+    Retruns:
+        tuple: x_train_transformed, y_train_transformed, x_test_transformed, y_test_transformed
+    """
+    c_cols, n_cols = get_columns_type(X_train)
+    preprocessor = ColumnTransformer(transformers=[('numerical',  KNNImputer(n_neighbors=2, weights='uniform'), n_cols),
+                                                   ('categorical', SimpleImputer(strategy='most_frequent'), c_cols)])
+    transformed_cols = n_cols + c_cols
+
+    y_imputer = SimpleImputer(strategy='median')
+    y_train_transformed = y_imputer.fit_transform(Y_train)
+    y_test_transformed = y_imputer.transform(Y_test)
+
+    y_train_transformed = y_train_transformed.mean(axis=1)
+    y_test_transformed = y_test_transformed.mean(axis=1)
+
+    preprocessor.fit(X_train)
+    x_train_transformed = pd.DataFrame(preprocessor.transform(X_train), columns=transformed_cols)
+    x_test_transformed = pd.DataFrame(preprocessor.transform(X_test), columns=transformed_cols)
+
+    # OneHotEncode for each category separately
+    x_train_transformed, x_test_transformed = one_hot_encoder(x_train_transformed, x_test_transformed, c_cols)
+
+    return x_train_transformed, y_train_transformed, x_test_transformed, y_test_transformed
+
+
 if __name__ == '__main__':
     args = utils.argument_parser()
 
@@ -21,31 +54,16 @@ if __name__ == '__main__':
     y_train = pd.read_csv(join(data_path, 'y_train.csv'), index_col=False)
     x_test = pd.read_csv(join(data_path, 'x_test.csv'), index_col=False)
     y_test = pd.read_csv(join(data_path, 'y_test.csv'), index_col=False)
-    x_cols = x_train.columns
-    c_cols, n_cols = get_columns_type(x_train)
-    preprocessor = ColumnTransformer(transformers=[('numerical',  KNNImputer(n_neighbors=2, weights='uniform'), n_cols),
-                                                   ('categorical', SimpleImputer(strategy='most_frequent'), c_cols)])
-    transformed_cols = n_cols + c_cols
-
-    y_imputer = SimpleImputer(strategy='median')
-    y_train_transformed = y_imputer.fit_transform(y_train)
-    y_test_transformed = y_imputer.transform(y_test)
-    y_train_end = y_train_transformed.mean(axis=1)
-    y_test_end = y_test_transformed.mean(axis=1)
-
-    preprocessor.fit(x_train)
-    x_train_transformed = pd.DataFrame(preprocessor.transform(x_train), columns=transformed_cols)
-    x_test_transformed = pd.DataFrame(preprocessor.transform(x_test), columns=transformed_cols)
-
-    # OneHotEncode for each category separately
-    x_train_transformed, x_test_transformed = one_hot_encoder(x_train_transformed, x_test_transformed, c_cols)
 
     param_grid = {'max_depth': [2, 4, 8, 9, 10, 16],
                   'max_leaf_nodes': [2, 4, 8, 16, 20, 22]}
 
     g_search = GridSearchCV(DecisionTreeRegressor(), param_grid=param_grid, scoring='r2')
 
-    g_search.fit(x_train_transformed, y_train_end)
+    x_train_transformed, y_train_transformed, x_test_transformed, y_test_transformed = preprocessing(x_train, y_train,
+                                                                                                     x_test, y_test)
+
+    g_search.fit(x_train_transformed, y_train_transformed)
     print(f"Best score {g_search.best_score_} with {g_search.best_estimator_}")
     best = g_search.best_estimator_
     print(f"Best parameters {best.get_params()}")
@@ -54,9 +72,9 @@ if __name__ == '__main__':
     print('Validation results')
     print('r2, mean poisson deviance, mse')
     folder = KFold(n_splits=5, random_state=10, shuffle=True)
-    for train_index, test_index in folder.split(x_train_transformed.to_numpy(), y_train_end):
-        fold_train_x, fold_train_y = x_train_transformed.iloc[train_index], y_train_end[train_index]
-        fold_test_x, fold_test_y = x_train_transformed.iloc[test_index], y_train_end[test_index]
+    for train_index, test_index in folder.split(x_train_transformed.to_numpy(), y_train_transformed):
+        fold_train_x, fold_train_y = x_train_transformed.iloc[train_index], y_train_transformed[train_index]
+        fold_test_x, fold_test_y = x_train_transformed.iloc[test_index], y_train_transformed[test_index]
 
         best.fit(fold_train_x, fold_train_y)
         y_pred = best.predict(fold_test_x)
@@ -69,21 +87,21 @@ if __name__ == '__main__':
     acum_res = acum_res / 5
     print(','.join(map(str, np.round(acum_res, 3))))
 
-    best.fit(x_train_transformed, y_train_end.ravel())
+    best.fit(x_train_transformed, y_train_transformed.ravel())
     print("Train score")
     y_pred = best.predict(x_train_transformed)
-    train_res = utils.calculate_regression_metrics(y_train_end, y_pred)
+    train_res = utils.calculate_regression_metrics(y_train_transformed, y_pred)
     print(','.join(map(str, train_res)))
 
     print("Test score")
     y_pred = best.predict(x_test_transformed)
-    test_res = utils.calculate_regression_metrics(y_test_end, y_pred)
+    test_res = utils.calculate_regression_metrics(y_test_transformed, y_pred)
     print(','.join(map(str, test_res)))
 
-    utils.plot_scattered_error(y_test_end, y_pred, 'Scattered error plot for decission tree',
+    utils.plot_scattered_error(y_test_transformed, y_pred, 'Scattered error plot for decission tree',
                                'Observations', 'IEMedia', args.save_figures, 'dtree')
 
-    utils.get_error_hist(y_test_end.ravel(), y_pred, 'Class', 'Count', 'Error count for decission tree',
+    utils.get_error_hist(y_test_transformed.ravel(), y_pred, 'Class', 'Count', 'Error count for decission tree',
                          args.save_figures, 'dtree')
 
     plt.figure(figsize=(20, 10))
